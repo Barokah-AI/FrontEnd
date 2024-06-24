15ba04cd4c47512d941c855a47c820a245e790ff
stefanymelda <juwitastefany13@gmail.com>
2024-06-24 22:48:39 +0700

    create komponen sidebar

---
+++ src/sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Barokah-AI</h2>
+      <p>Item 1</p>
+      <p>Item 2</p>
+      <p>Item 3</p>
+      {/* Tambahkan konten sidebar lainnya di sini */}
+    </div>
+  );
+}
+
+export default Sidebar;
