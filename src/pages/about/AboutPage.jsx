a335902b5ed9552ba556c141b0692b812e4dff00
stefanymelda <juwitastefany13@gmail.com>
2024-07-10 13:06:42 +0700

    Create AboutPage and FeaturesPage components

---
+++ src/pages/about/AboutPage.jsx
@@ -0,0 +1,17 @@
+import AboutSectionOne from "/src/components/AboutSectionOne";
+import AboutSectionTwo from "/src/components/AboutSectionTwo";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Header />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Footer />
+    </>
+  );
+};
+
+export default AboutPage;
