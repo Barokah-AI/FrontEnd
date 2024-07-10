84431c016ee883421d74f3c433c04c1ab2d020d0
stefanymelda <juwitastefany13@gmail.com>
2024-07-10 12:40:29 +0700

    Add new pages for about, profile, and features

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 
 import Index from "./pages/Index";
 import ChatPage from "./pages/chatPage/ChatPage";
+import AboutPage from "./pages/about/AboutPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import FeaturesPage from "./pages/features/FeaturesPage";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </>
   );
