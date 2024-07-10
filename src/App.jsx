46f908e2c5258e3f365e762ea217a554b503dc57
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-10 09:44:28 +0700

    Creatte Header and Hero Section Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Index from "./pages/Index";
-import ChatPage from "./pages/chatPage/ChatPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </>
   );
---
+++ src/pages/Index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ScrollUp from "../components/ScrollUp";
+import Hero from "../components/Hero";
+import Header from "../components/Header";
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <ScrollUp />
+      <Hero />
+    </>
+  );
+};
+
+export default Index;
