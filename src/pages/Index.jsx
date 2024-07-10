fcc56484139c9efaaa5474f334cad0fe3e915b2d
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-10 09:53:02 +0700

    Create Content Landing Page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ScrollUp from "../components/ScrollUp";
 import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Brands from "../components/Brands";
+import AboutSectionOne from "../components/AboutSectionOne";
+import AboutSectionTwo from "../components/AboutSectionTwo";
+import Testimonials from "../components/Testimonials";
+import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -10,6 +16,12 @@ const Index = () => {
       <Header />
       <ScrollUp />
       <Hero />
+      <Features />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Contact />
       <Footer />
     </>
   );
