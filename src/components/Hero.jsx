a92876d1391e2caabc35912c4fbc356474db87de
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-10 14:34:35 +0700

    edit home hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,10 +27,10 @@ const Hero = () => {
                     Chatbot
                   </Link>
                   <Link
-                    to="https://github.com/NextJSTemplates/startup-nextjs"
+                    to="https://github.com/Barokah-AI/FrontEnd"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
-                    Star on GitHub
+                    Start on GitHub
                   </Link>
                 </div>
               </div>
