990906e73c8d71ce7d67d66f9bea75177ec53b99
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-12 22:07:16 +0700

    Update About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import SectionTitle from "./SectionTittle";
+import { motion } from "framer-motion";
 
 const checkIcon = (
   <svg width="16" height="13" viewBox="0 0 16 13" className="fill-current">
@@ -16,23 +18,37 @@ const About = () => {
     </p>
   );
 
+  const textVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const imageVariants = {
+    hidden: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
     <section id="about">
       <div className="pt-16 md:pt-20 lg:pt-28">
         <div className="container">
           <div className="pb-16 md:pb-20 lg:pb-28">
             <div className="-mx-4 flex flex-wrap items-center">
-              <div className="w-full px-4 lg:w-1/2">
+              <motion.div
+                className="w-full px-4 lg:w-1/2"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ delay: 0.5, duration: 1 }}
+                variants={textVariants}
+              >
                 <SectionTitle
                   title="Barokah-AI"
                   paragraph="BarokahAI is a digital friend who is here to answer all your questions quickly and precisely. Using artificial intelligence, BarokahAI provides the information you need. With advanced features and a user-friendly interface, BarokahAI is here to provide safe and reliable support. Let's explore new knowledge and find ease in every interaction with BarokahAI!"
                   mb="44px"
                 />
 
-                <div
-                  className="mb-12 max-w-[570px] lg:mb-0"
-                  data-wow-delay=".15s"
-                >
+                <div className="mb-12 max-w-[570px] lg:mb-0">
                   <div className="mx-[-12px] flex flex-wrap">
                     <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
                       <List text="Natural Language Processing" />
@@ -47,17 +63,24 @@ const About = () => {
                     </div>
                   </div>
                 </div>
-              </div>
+              </motion.div>
 
-              <div className="w-full px-4 lg:w-1/2">
+              <motion.div
+                className="w-full px-4 lg:w-1/2"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ delay: 0.5, duration: 1 }}
+                variants={imageVariants}
+              >
                 <div className="relative mx-auto aspect-w-25 aspect-h-24 max-w-[500px] lg:mr-0">
                   <img
-                    src="https://github.com/Barokah-AI/FrontEnd/assets/94734096/d42f6324-a33d-4103-8e60-9b7288c3fa74"
+                    src="/images/about/about-section-1.png"
                     alt="about-image"
                     className="mx-auto max-w-full drop-shadow-three dark:drop-shadow-none lg:mr-0"
                   />
                 </div>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
@@ -65,11 +88,15 @@ const About = () => {
       <div className="py-16 md:py-20 lg:py-28">
         <div className="container">
           <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 lg:w-1/2">
-              <div
-                className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-                data-wow-delay=".15s"
-              >
+            <motion.div
+              className="w-full px-4 lg:w-1/2"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ delay: 0.5, duration: 1 }}
+              variants={imageVariants}
+            >
+              <div className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0">
                 <img
                   src="https://github.com/Barokah-AI/FrontEnd/assets/104063079/cf74a9a2-5084-4310-9817-b201af54cec8"
                   alt="about image"
@@ -77,8 +104,15 @@ const About = () => {
                   className="drop-shadow-three dark:drop-shadow-none"
                 />
               </div>
-            </div>
-            <div className="w-full px-4 lg:w-1/2">
+            </motion.div>
+            <motion.div
+              className="w-full px-4 lg:w-1/2"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ delay: 0.5, duration: 1 }}
+              variants={textVariants}
+            >
               <div className="max-w-[470px]">
                 <div className="mb-9">
                   <h2 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
@@ -106,7 +140,7 @@ const About = () => {
                   </p>
                 </div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
