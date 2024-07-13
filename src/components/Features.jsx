3d24a8a7f7cb3409fd0fcfb42059c6bb8eeeaf8a
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-13 18:46:44 +0700

    Update Feature Section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,20 +2,48 @@ import React from "react";
 import featuresData from "../data/featuresData";
 import SectionTitle from "./SectionTittle";
 import SingleFeature from "./SingleFeature";
+import { motion } from "framer-motion";
 
 const Features = () => {
+  const titleVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <>
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
-          <SectionTitle
-            title="Main Features"
-            paragraph="Explore the Key Features of BarokahAI and Discover Ease and Excellence in Every Interaction. We hope your experience here will be both beneficial and satisfying."
-            center
-          />
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={titleVariants}
+            viewport={{ once: true }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
+            <SectionTitle
+              title="Main Features"
+              paragraph="Explore the Key Features of BarokahAI and Discover Ease and Excellence in Every Interaction. We hope your experience here will be both beneficial and satisfying."
+              center
+            />
+          </motion.div>
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
-            {featuresData.map((feature) => (
-              <SingleFeature key={feature.id} feature={feature} />
+            {featuresData.map((feature, index) => (
+              <motion.div
+                key={feature.id}
+                initial="hidden"
+                whileInView="visible"
+                variants={itemVariants}
+                viewport={{ once: true }}
+                transition={{ delay: 0.5 + index * 0.2, duration: 0.5 }}
+              >
+                <SingleFeature feature={feature} />
+              </motion.div>
             ))}
           </div>
         </div>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,27 +2,25 @@ import ScrollUp from "../components/ScrollUp";
 import Hero from "../components/Hero";
 import ScrollToTop from "../components/ScrollToTop";
 import Features from "../components/Features";
-import Brands from "../components/Brands";
 import About from "../components/About";
-import Testimonials from "../components/Testimonials";
+import Team from "../components/Team";
 import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Index = () => {
   return (
-    <>
+    <div className="overflow-hidden">
       <Header />
       <ScrollUp />
       <Hero />
       <About />
-      <Brands />
       <Features />
-      <Testimonials />
+      <Team />
       <Contact />
       <Footer />
       <ScrollToTop />
-    </>
+    </div>
   );
 };
 
