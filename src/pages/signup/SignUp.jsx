3c68d5b4902c8fd91dad272f81bc6e4486419b32
stefanymelda <juwitastefany13@gmail.com>
2024-07-15 21:00:15 +0700

    Update placeholder text

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -103,7 +103,7 @@ const SignupPage = () => {
                     <input
                       type="email"
                       name="email"
-                      placeholder="Enter your Email"
+                      placeholder="Enter your email"
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                     />
                   </div>
@@ -118,7 +118,7 @@ const SignupPage = () => {
                     <input
                       type="password"
                       name="password"
-                      placeholder="Enter your Password"
+                      placeholder="Enter your password"
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                     />
                   </div>
@@ -174,7 +174,7 @@ const SignupPage = () => {
                 </form>
                 <p className="text-center text-base font-medium text-body-color">
                   Already using BarokahAI?{" "}
-                  <Link href="/signin" className="text-primary hover:underline">
+                  <Link to="/signin" className="text-primary hover:underline">
                     Sign in
                   </Link>
                 </p>
