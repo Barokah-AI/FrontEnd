739db5d10c676a674f5a832802f746820fc8b709
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-19 00:50:24 +0700

    update SignIn

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -13,7 +13,7 @@ const SigninPage = () => {
             <div className="w-full px-4">
               <div className="shadow-three mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
                 <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
-                  Sign in to your account
+                  SignIn to your account
                 </h3>
                 <p className="mb-11 text-center text-base font-medium text-body-color">
                   Please login to start more conversations.
