9b1fd3ffaf26b4ee95d9888d207597d95a2b90db
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-19 13:44:37 +0700

    Update  Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-full px-4">
+                  {/* <div className="w-full px-4">
                     <div className="mb-8">
                       <label
                         htmlFor="message"
@@ -63,7 +63,7 @@ const Contact = () => {
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       ></textarea>
                     </div>
-                  </div>
+                  </div> */}
                   <div className="w-full px-4">
                     <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
                       Send Message
