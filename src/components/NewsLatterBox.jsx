7849009c612ed31dbd788375b1fef5d3e5d034b5
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-19 13:43:22 +0700

    Update NewsLatterBox component

--- src/components/NewsLatterBox.jsx
+++ src/components/NewsLatterBox.jsx
@@ -5,7 +5,7 @@ const NewsLatterBox = () => {
         Subscribe to receive future updates
       </h3>
       <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
-        Stay informed about new features, updates, and exclusive content from Barokah AI. Subscribe now and never miss an update!
+        
       </p>
       <div>
         <input
@@ -14,7 +14,7 @@ const NewsLatterBox = () => {
           placeholder="Enter your name"
           className="border-stroke mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
         />
-        <input
+        {/* <input
           type="email"
           name="email"
           placeholder="Enter your email"
@@ -24,9 +24,9 @@ const NewsLatterBox = () => {
           type="submit"
           value="Subscribe"
           className="mb-5 flex w-full cursor-pointer items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
-        />
+        /> */}
         <p className="text-center text-base leading-relaxed text-body-color dark:text-body-color-dark">
-          No spam guaranteed. We value your privacy, So please don’t send any spam mail.
+          No spam guaranteed. 
         </p>
       </div>
 
