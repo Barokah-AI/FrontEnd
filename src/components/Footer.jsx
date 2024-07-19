0c7619c10bcb7a91e5233169486753aa8b92d24a
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-19 13:34:51 +0700

    update footer link styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -131,12 +131,12 @@ const Footer = () => {
                 </h2>
                 <ul>
                   <li>
-                    <Link
-                      to="/"
-                      className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
+                  <a
+                      href="#"
+                      className="mb-2 inline-block text-base leading-loose text-body-color hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       TOS
-                    </Link>
+                    </a>
                   </li>
                   <li>
                     <Link
