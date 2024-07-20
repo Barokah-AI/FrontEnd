9f3f65f1535611c3e4f1f66874130bc9e8a4ea7a
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 19:19:38 +0700

    add animation

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -49,7 +49,12 @@ const NotFoundPage = () => {
           <div className="flex flex-wrap -mx-4">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[530px] text-center">
-                <div className="mx-auto text-center mb-9">
+                <motion.div
+                  className="mx-auto text-center mb-9"
+                  initial="hidden"
+                  animate="visible"
+                  variants={svgVariants}
+                >
                   <svg
                     className="w-full mx-auto text-center"
                     height="210"
@@ -173,7 +178,7 @@ const NotFoundPage = () => {
                       </radialGradient>
                     </defs>
                   </svg>
-                </div>
+                </motion.div>
                 <h3 className="mb-4 text-3xl font-bold text-black dark:text-white sm:text-4xl">
                   Sorry, the page can’t be found
                 </h3>
