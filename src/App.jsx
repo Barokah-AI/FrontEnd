35548d48cb72f9804c8bb90fc49d45cbfbddb479
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 19:08:04 +0700

    Add route for NotFoundPage component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
