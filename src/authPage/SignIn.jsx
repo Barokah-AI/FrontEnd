e94e120dae538eb5849e66b2b983853b2e3720ec
stefanymelda <juwitastefany13@gmail.com>
2024-07-20 14:12:08 +0700

    fix edit signin

--- src/authPage/SignIn.jsx
+++ src/authPage/SignIn.jsx
@@ -52,7 +52,7 @@ const SigninPage = () => {
                       </defs>
                     </svg>
                   </span>
-                  Sign in with Gmail
+                  Sign in with Google
                 </button>
 
                 <button className="border-stroke dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none">
@@ -67,7 +67,7 @@ const SigninPage = () => {
                       <path d="M32 1.7998C15 1.7998 1 15.5998 1 32.7998C1 46.3998 9.9 57.9998 22.3 62.1998C23.9 62.4998 24.4 61.4998 24.4 60.7998C24.4 60.0998 24.4 58.0998 24.3 55.3998C15.7 57.3998 13.9 51.1998 13.9 51.1998C12.5 47.6998 10.4 46.6998 10.4 46.6998C7.6 44.6998 10.5 44.6998 10.5 44.6998C13.6 44.7998 15.3 47.8998 15.3 47.8998C18 52.6998 22.6 51.2998 24.3 50.3998C24.6 48.3998 25.4 46.9998 26.3 46.1998C19.5 45.4998 12.2 42.7998 12.2 30.9998C12.2 27.5998 13.5 24.8998 15.4 22.7998C15.1 22.0998 14 18.8998 15.7 14.5998C15.7 14.5998 18.4 13.7998 24.3 17.7998C26.8 17.0998 29.4 16.6998 32.1 16.6998C34.8 16.6998 37.5 16.9998 39.9 17.7998C45.8 13.8998 48.4 14.5998 48.4 14.5998C50.1 18.7998 49.1 22.0998 48.7 22.7998C50.7 24.8998 51.9 27.6998 51.9 30.9998C51.9 42.8998 44.5 45.3998 37.6 46.1998C38.7 47.0998 39.7 48.8998 39.7 51.5998C39.7 55.3998 39.6 58.2998 39.6 60.7998C39.6 61.4998 40.1 62.4998 41.8 62.1998C54.1 57.9998 63 46.3998 63 32.7998C63 15.5998 49 1.7998 32 1.7998Z" />
                     </svg>
                   </span>
-                  Sign in with Linkedin
+                  Sign in with GitHub
                 </button>
 
                 <div className="mb-8 flex items-center justify-center">
