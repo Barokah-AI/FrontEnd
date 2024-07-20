02e3f8f140b7c04cceac9470d490ffdbb5849b9a
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 19:30:13 +0700

    update signin

--- src/authPage/SignIn.jsx
+++ src/authPage/SignIn.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const SigninPage = () => {
   return (
     <>
-      <Header /> {/* Include Header component */}
+      <Header />
       <section className="relative z-10 overflow-hidden pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[180px]">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
-              <div className="shadow-three mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
+              <motion.div
+                className="shadow-three mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]"
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+              >
                 <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
                   Sign in to your account
                 </h3>
@@ -64,20 +70,17 @@ const SigninPage = () => {
                       viewBox="0 0 64 64"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <path d="M32 1.7998C15 1.7998 1 15.5998 1 32.7998C1 46.3998 9.9 57.9998 22.3 62.1998C23.9 62.4998 24.4 61.4998 24.4 60.7998C24.4 60.0998 24.4 58.0998 24.3 55.3998C15.7 57.3998 13.9 51.1998 13.9 51.1998C12.5 47.6998 10.4 46.6998 10.4 46.6998C7.6 44.6998 10.5 44.6998 10.5 44.6998C13.6 44.7998 15.3 47.8998 15.3 47.8998C18 52.6998 22.6 51.2998 24.3 50.3998C24.6 48.3998 25.4 46.9998 26.3 46.1998C19.5 45.4998 12.2 42.7998 12.2 30.9998C12.2 27.5998 13.5 24.8998 15.4 22.7998C15.1 22.0998 14 18.8998 15.7 14.5998C15.7 14.5998 18.4 13.7998 24.3 17.7998C26.8 17.0998 29.4 16.6998 32.1 16.6998C34.8 16.6998 37.5 16.9998 39.9 17.7998C45.8 13.8998 48.4 14.5998 48.4 14.5998C50.1 18.7998 49.1 22.0998 48.7 22.7998C50.7 24.8998 51.9 27.6998 51.9 30.9998C51.9 42.8998 44.5 45.3998 37.6 46.1998C38.7 47.0998 39.7 48.8998 39.7 51.5998C39.7 55.3998 39.6 58.2998 39.6 60.7998C39.6 61.4998 40.1 62.4998 41.8 62.1998C54.1 57.9998 63 46.3998 63 32.7998C63 15.5998 49 1.7998 32 1.7998Z" />
+                      <path d="M32 1.7998C15 1.7998 1 15.5998 1 32.7998C1 46.3998 9.9 57.9998 22.3 62.1998C23.9 62.4998 24.4 61.4998 24.4 60.7998C24.4 60.0998 24.4 58.0998 24.3 55.3998C15.7 57.3998 13.9 51.1998 13.9 51.1998C12.5 47.6998 10.4 46.6998 10.4 46.6998C7.6 44.6998 10.5 44.6998 10.5 44.6998C13.6 44.7998 15.3 47.8998 15.3 47.8998C18 52.6998 22.6 51.2998 24.3 50.3998C24.6 48.3998 25.4 46.9998 26.3 46.1998C19.5 45.4998 12.2 42.7998 12.2 30.9998C12.2 27.5998 13.5 24.8998 15.4 22.7998C15.1 22.0998 14 18.8998 15.7 14.5998C15.7 14.5998 18.4 13.7998 24.3 17.7998C26.8 17.0998 29.4 16.6998 32.1 16.6998C34.8 16.6998 37.4 17.0998 39.9 17.7998C45.8 13.7998 48.5 14.5998 48.5 14.5998C50.2 18.8998 49.1 22.0998 48.8 22.7998C50.7 24.8998 52 27.5998 52 30.9998C52 42.8998 45.6 45.4998 38.8 46.1998C40 47.0998 41 49.1998 41 52.1998C41 56.4998 41 59.9998 41 60.7998C41 61.4998 41.4 62.4998 43 62.1998C55.7 57.9998 64 46.3998 64 32.7998C64 15.5998 49 1.7998 32 1.7998Z" />
                     </svg>
                   </span>
-                  Sign in with GitHub
+                  Sign in with Github
                 </button>
 
-                <div className="mb-8 flex items-center justify-center">
-                  <span className="h-[1px] w-full max-w-[150px] bg-body-color"></span>
-                  <p className="px-5 text-center text-base font-medium text-body-color">
-                    OR
-                  </p>
-                  <span className="h-[1px] w-full max-w-[150px] bg-body-color"></span>
+                <div className="mb-6 flex items-center justify-center">
+                  <span className="h-[1px] w-full bg-body-color"></span>
+                  <p className="px-5 text-base text-body-color">or</p>
+                  <span className="h-[1px] w-full bg-body-color"></span>
                 </div>
-
                 <form>
                   <div className="mb-8">
                     <label
@@ -89,11 +92,11 @@ const SigninPage = () => {
                     <input
                       type="email"
                       name="email"
-                      placeholder="example@gmail.com"
-                      className="border-[f0f0f0] w-full rounded-md border bg-[#FCFDFE] py-3 px-6 text-base text-body-color placeholder-body-color outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#2C303B] dark:text-body-color-dark dark:placeholder-body-color-dark dark:focus:border-primary"
+                      placeholder="example@example.com"
+                      className="border-stroke dark:bg-form-input dark:border-form-strokedark dark:focus:border-primary dark:text-body-color-dark focus:border-primary w-full rounded-lg border bg-transparent py-3 px-6 text-base font-medium outline-none transition"
                     />
                   </div>
-                  <div className="mb-8">
+                  <div className="mb-6">
                     <label
                       htmlFor="password"
                       className="mb-3 block text-sm font-medium text-dark dark:text-white"
@@ -104,143 +107,57 @@ const SigninPage = () => {
                       type="password"
                       name="password"
                       placeholder="Enter your Password"
-                      className="border-[f0f0f0] w-full rounded-md border bg-[#FCFDFE] py-3 px-6 text-base text-body-color placeholder-body-color outline-none focus:border-primary focus-visible:shadow-none dark:bg-[#2C303B] dark:text-body-color-dark dark:placeholder-body-color-dark dark:focus:border-primary"
+                      className="border-stroke dark:bg-form-input dark:border-form-strokedark dark:focus:border-primary dark:text-body-color-dark focus:border-primary w-full rounded-lg border bg-transparent py-3 px-6 text-base font-medium outline-none transition"
                     />
                   </div>
-                  <div className="mb-8 flex items-center justify-between">
-                    <div className="flex items-center">
-                      <input
-                        type="checkbox"
-                        id="checkboxLabelOne"
-                        className="mr-3 h-5 w-5 accent-primary"
-                      />
-                      <label
-                        htmlFor="checkboxLabelOne"
-                        className="text-sm font-medium text-body-color"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                    <Link
-                      to="/forgot-password"
-                      className="text-sm font-medium text-primary hover:underline"
-                    >
-                      Forget Password?
-                    </Link>
-                  </div>
-                  <div className="mb-6">
-                    <button
+                  <div className="mb-10">
+                    <input
                       type="submit"
-                      className="w-full rounded-md bg-primary py-3 px-5 text-base text-white transition hover:bg-opacity-90"
-                    >
-                      Sign In
-                    </button>
+                      value="Sign In"
+                      className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-3 text-base font-medium text-white transition hover:bg-opacity-90"
+                    />
                   </div>
                 </form>
-
                 <p className="text-center text-base font-medium text-body-color">
                   Don’t you have an account?
                   <Link to="/signup" className="text-primary hover:underline">
                     Sign Up
                   </Link>
                 </p>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
-        <div className="absolute right-0 top-0 z-[-1]">
+        <div className="absolute bottom-0 right-0 z-[-1]">
           <svg
             width="1440"
-            height="969"
-            viewBox="0 0 1440 969"
+            height="886"
+            viewBox="0 0 1440 886"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <mask
-              id="mask0_95:1005"
-              style={{ maskType: "alpha" }}
-              maskUnits="userSpaceOnUse"
-              x="0"
-              y="0"
-              width="1440"
-              height="969"
-            >
-              <rect width="1440" height="969" fill="#060821" />
-            </mask>
-            <g mask="url(#mask0_95:1005)">
-              <path
-                opacity="0.1"
-                d="M1086.96 297.868C1186.26 231.898 1257.17 116.619 1243.09 2.2485L948.583 113.057C960.504 171.907 1025.88 232.58 1086.96 297.868Z"
-                fill="url(#paint0_linear_95:1005)"
-              />
-              <path
-                opacity="0.1"
-                d="M1157.93 344.603C1304.83 344.603 1424.7 276.116 1424.7 194.332C1424.7 112.548 1304.83 44.0618 1157.93 44.0618C1011.03 44.0618 891.113 112.548 891.113 194.332C891.113 276.116 1011.03 344.603 1157.93 344.603Z"
-                stroke="url(#paint1_linear_95:1005)"
-                strokeWidth="0.5"
-              />
-              <path
-                opacity="0.1"
-                d="M429.507 483.21C529.618 552.684 654.349 518.282 676.031 434.442L382.021 388.661C373.92 448.731 363.988 441.601 429.507 483.21Z"
-                fill="url(#paint2_linear_95:1005)"
-              />
-              <path
-                opacity="0.1"
-                d="M361.477 542.694C416.997 597.974 530.161 625.626 624.691 571.228C719.221 516.829 748.413 402.848 692.894 347.569C637.374 292.289 524.21 264.638 429.68 319.036C335.15 373.435 305.958 487.416 361.477 542.694Z"
-                stroke="url(#paint3_linear_95:1005)"
-                strokeWidth="0.5"
-              />
-            </g>
+            <path
+              opacity="0.5"
+              d="M1440 0V886H0V0C57.2886 4.9269 121.122 15.6284 192 35.1786C319.931 72.2786 323.024 120.98 436.971 173.214C550.918 225.448 682.593 184.915 754 173.214C825.407 161.513 949.84 130.318 1034 152.835C1118.16 175.352 1176.29 259.388 1285 330.028C1358.89 376.668 1418.24 405.145 1440 413.035Z"
+              fill="url(#paint0_linear_1179_5)"
+            />
             <defs>
               <linearGradient
-                id="paint0_linear_95:1005"
-                x1="913.479"
-                y1="-33.0217"
-                x2="1242.28"
-                y2="297.016"
+                id="paint0_linear_1179_5"
+                x1="720"
+                y1="0"
+                x2="720"
+                y2="886"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-              <linearGradient
-                id="paint1_linear_95:1005"
-                x1="1394.7"
-                y1="194.332"
-                x2="881.197"
-                y2="194.332"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-              <linearGradient
-                id="paint2_linear_95:1005"
-                x1="700.014"
-                y1="400.886"
-                x2="352.583"
-                y2="504.967"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
-              </linearGradient>
-              <linearGradient
-                id="paint3_linear_95:1005"
-                x1="715.53"
-                y1="337.057"
-                x2="291.322"
-                y2="563.651"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop stopColor="#3056D3" stopOpacity="0.09" />
+                <stop offset="1" stopColor="#C4C4C4" stopOpacity="0" />
               </linearGradient>
             </defs>
           </svg>
         </div>
       </section>
-      <Footer /> {/* Include Footer component */}
+      <Footer />
     </>
   );
 };
