35cc1c014706e30089332ac759ff1e52d42a64d0
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 20:13:02 +0700

    add logout function

---
+++ src/pages/chatPage/Logout.jsx
@@ -0,0 +1,13 @@
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
+export const useLogout = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    Cookies.remove("authToken");
+    navigate("/signin");
+  };
+
+  return logout;
+};
