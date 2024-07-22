c356f4b5388cb8fdefc5ab48f049fea95b1e4761
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-22 22:50:27 +0700

    Update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -124,7 +124,7 @@ const Footer = () => {
               </div>
             </div>
 
-            {/* <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
                   Terms
@@ -156,7 +156,7 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-            </div> */}
+            </div>
 
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-3/12">
               <div className="mb-12 lg:mb-16">
