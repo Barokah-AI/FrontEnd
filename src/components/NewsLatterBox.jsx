f6e86c18ce62c6bdd5d1b36c331caf2b80a5ae55
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-23 21:52:26 +0700

    update news latter box

--- src/components/NewsLatterBox.jsx
+++ src/components/NewsLatterBox.jsx
@@ -26,7 +26,7 @@ const NewsLatterBox = () => {
           className="mb-5 flex w-full cursor-pointer items-center justify-center rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
         />
         <p className="text-center text-base leading-relaxed text-body-color dark:text-body-color-dark">
-          No szpam guaranteed. We value your privacy, So please don’t send any spam mail.
+          No spam guaranteed. We value your privacy, So please don’t send any spam mail.
         </p>
       </div>
 
@@ -46,13 +46,13 @@ const NewsLatterBox = () => {
             <defs>
               <linearGradient
                 id="paint0_linear_1028_600"
-                x1="-20.3585"
-                y1="55.1545"
-                x2="35.165"
-                y2="15.3505"
+                x1="-18.3187"
+                y1="55.1044"
+                x2="37.161"
+                y2="15.3509"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stopColor="light" stopOpacity="0.65" />
+                <stop stopColor="light" stopOpacity="0.62" />
                 <stop offset="1" stopColor="light" stopOpacity="0" />
               </linearGradient>
             </defs>
@@ -61,9 +61,9 @@ const NewsLatterBox = () => {
 
         <span className="absolute bottom-24 left-1.5">
           <svg
-            width="40"
-            height="35"
-            viewBox="0 0 40 35"
+            width="39"
+            height="32"
+            viewBox="0 0 39 32"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
@@ -87,7 +87,7 @@ const NewsLatterBox = () => {
           </svg>
         </span>
 
-        {/* <span className="absolute right-2 top-[140px]">
+        <span className="absolute right-2 top-[140px]">
           <svg
             width="38"
             height="38"
@@ -113,7 +113,7 @@ const NewsLatterBox = () => {
               </linearGradient>
             </defs>
           </svg>
-        </span> */}
+        </span>
 
         <span className="absolute right-0 top-0">
           <svg
