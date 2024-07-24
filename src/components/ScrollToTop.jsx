438f616634c267208bdeb29b743a2f9592c63a5e
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-24 14:13:47 +0700

    code in ScrollToTop component

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -12,20 +12,20 @@ export default function ScrollToTop() {
     });
   };
 
-  useEffect(() => {
-    // Button is displayed after scrolling for 500 pixels
-    const toggleVisibility = () => {
-      if (window.pageYOffset > 300) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
+  // useEffect(() => {
+  //   // Button is displayed after scrolling for 500 pixels
+  //   const toggleVisibility = () => {
+  //     if (window.pageYOffset > 300) {
+  //       setIsVisible(true);
+  //     } else {
+  //       setIsVisible(false);
+  //     }
+  //   };
 
-    window.addEventListener("scroll", toggleVisibility);
+  //   window.addEventListener("scroll", toggleVisibility);
 
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
+  //   return () => window.removeEventListener("scroll", toggleVisibility);
+  // }, []);
 
   return (
     <div className="fixed bottom-8 right-8 z-[99]">
