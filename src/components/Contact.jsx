90f80c1640caf2c457fb78f9c0aaa8bd5f9d700c
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-24 14:19:24 +0700

    Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
                       >
                         Full Name
                       </label>
-                      {/* <input
+                      <input
                         type="text"
                         placeholder="Input your full name here"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
@@ -47,7 +47,7 @@ const Contact = () => {
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
                     </div>
-                  </div> */}
+                  </div>
                   <div className="w-full px-4">
                     <div className="mb-8">
                       <label
