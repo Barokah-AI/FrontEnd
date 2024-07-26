464748991e687ee0aafc72aa1a45127802584ef0
dani ferdinan <daniferdinandall@gmail.com>
2024-07-26 17:29:01 +0700

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,10 @@ const Hero = () => {
                   opacity: 0,
                   x: -100 
                 }}
-                animate={{ opacity: 1, x: 0 }}
+                animate={{ 
+                  opacity: 1, 
+                  x: 0 
+                }}
                 transition={{ duration: 1 }}
               >
                 <motion.h1
