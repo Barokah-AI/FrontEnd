15158864410d94883bb5a18cf18bf578c579c1a2
dani ferdinan <daniferdinandall@gmail.com>
2024-07-26 17:14:32 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
 
             <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
-                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">Terms and Condition</h2>
+                <h2 className="mb-10 text-xl font-bold text-black dark:text-white">Terms & Condition</h2>
                 <ul>
                   <li>
                     <a href="#" className="mb-2 inline-block text-base leading-loose text-body-color hover:text-primary dark:text-body-color-dark dark:hover:text-primary">
