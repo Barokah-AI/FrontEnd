f23c20e5a4be8320030fe8cdc23c9cfe761e38a2
dani ferdinan <daniferdinandall@gmail.com>
2024-07-26 17:30:57 +0700

    update newsletterbox

--- src/components/NewsLatterBox.jsx
+++ src/components/NewsLatterBox.jsx
@@ -11,7 +11,7 @@ const NewsLatterBox = () => {
         <input
           type="text"
           name="name"
-          placeholder="Enter your name"
+          placeholder="Enter your full name"
           className="border-stroke mb-4 w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
         />
         <input
