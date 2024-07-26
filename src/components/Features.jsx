1d528e677b6912aa958d1ea51d1c89f40ef1e92c
dani ferdinan <daniferdinandall@gmail.com>
2024-07-26 17:09:33 +0700

    update feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,7 @@ import SingleFeature from "./SingleFeature";
 import { motion } from "framer-motion";
 
 const Features = () => {
-  const titleVariants = {
+  const titleVariant = {
     hidden: { 
       opacity: 0, 
       y: -50 
@@ -30,7 +30,7 @@ const Features = () => {
     <>
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
-          <motion.div initial="hidden" whileInView="visible" variants={titleVariants} viewport={{ once: true }} transition={{ delay: 0.5, duration: 1 }}>
+          <motion.div initial="hidden" whileInView="visible" variants={titleVariant} viewport={{ once: true }} transition={{ delay: 0.5, duration: 1 }}>
             <SectionTitle title="Main Features" paragraph="Explore the Key Features of BarokahAI and Discover Ease and Excellence in Every Interaction. We hope your experience here will be both beneficial and satisfying." center />
           </motion.div>
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
