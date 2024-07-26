5df869d8a25bf47cc1b933599782b329eda220de
dani ferdinan <daniferdinandall@gmail.com>
2024-07-26 16:31:46 +0700

    udpate aboutpage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,16 @@ const checkIcon = (
 );
 
 const About = () => {
-  const List = ({ text }) => (
-    <p className="mb-5 flex items-center text-lg font-medium text-body-color">
-      <span className="mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-        {checkIcon}
-      </span>
-      {text}
-    </p>
-  );
+  const List = ({ text }) => {
+    return (
+      <p className="mb-5 flex items-center text-lg font-medium text-body-color">
+        <span className="mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
+          {checkIcon}
+        </span>
+        {text}
+      </p>
+    );
+  };
 
   const textVariants = {
     hidden: { opacity: 0, x: -50 },
