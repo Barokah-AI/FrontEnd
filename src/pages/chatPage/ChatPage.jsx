b28a70ee16012e10dfcfd026ec3333f1e30fcef8
dani ferdinan <daniferdinandall@gmail.com>
2024-07-26 16:17:18 +0700

    update chatpage

--- src/pages/chatPage/ChatPage.jsx
+++ src/pages/chatPage/ChatPage.jsx
@@ -157,7 +157,7 @@ const ChatPage = () => {
             <div className="p-4 border-t border-gray-200 dark:border-neutral-700">
               <label className="flex justify-between items-center gap-x-3 py-2 px-3 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-300" htmlFor="my_modal_6">
                 Sign out
-                <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                   <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
                   <polyline points="10 17 15 12 10 7" />
                   <line x1="15" x2="3" y1="12" y2="12" />
