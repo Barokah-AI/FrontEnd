ae3867a3f7e0885cee4086b11daf9b7424478f6d
erditona <erditonaushaadam@gmail.com>
2024-07-26 19:52:29 +0700

    refactor: Replace text in About.jsx with updated snippet

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -64,7 +64,7 @@ const About = () => {
                 </div>
               </motion.div>
 
-              Replace the text with the following snippet 
+              {/* Replace the text with the following snippet  */}
               <motion.div className="w-full px-4 lg:w-1/2" initial="hidden" whileInView="visible" viewport={{ once: true }} transition={{ delay: 0.5, duration: 1 }} variants={imageVariants}>
                 <div className="relative mx-auto aspect-w-25 aspect-h-24 max-w-[500px] lg:mr-0">
                   <img src="/images/about/pic-about.svg" alt="about-image" className="mx-auto max-w-full drop-shadow-three dark:drop-shadow-none lg:mr-0" />
