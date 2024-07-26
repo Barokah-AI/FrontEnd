27cb1cce83b9e18cf3361907d308c5528e4b0408
erditona <erditonaushaadam@gmail.com>
2024-07-26 17:21:07 +0700

    refactor: Update teamData in About.jsx

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
-import Cookies from "js-cookie";
+// import React from "react";
+// import { Navigate, useLocation } from "react-router-dom";
+// import Cookies from "js-cookie";
 
-const PrivateRoute = ({ children }) => {
-  const location = useLocation();
-  const authToken = Cookies.get("authToken");
+// const PrivateRoute = ({ children }) => {
+//   const location = useLocation();
+//   const authToken = Cookies.get("authToken");
 
-  if (!authToken) {
-    return <Navigate to="/signin" state={{ from: location }} replace />;
-  }
+//   if (!authToken) {
+//     return <Navigate to="/signin" state={{ from: location }} replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 
-export default PrivateRoute;
+// export default PrivateRoute;
