d0981f9c9f46c5c6714ed23631176fb3b4a48258
AdeCandra12 <ade.can321@gmail.com>
2024-08-01 23:28:37 +0700

    Update SignUp page placeholder for full name input

--- src/authPage/SignUp.jsx
+++ src/authPage/SignUp.jsx
@@ -47,7 +47,7 @@ const SignupPage = () => {
                     <input
                       type="text"
                       name="namalengkap"
-                      placeholder="Enter your full name"
+                      placeholder="Full Name"
                       value={namalengkap}
                       onChange={(e) => setNamalengkap(e.target.value)}
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
