e0b8d0f5074fab7175959fbe0b844da8667addaf
AdeCandra12 <ade.can321@gmail.com>
2024-08-02 13:11:07 +0700

    Update Contact component placeholder text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
                       </label>
                       <input
                         type="text"
-                        placeholder="Input full name"
+                        placeholder="Input full name Here"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
                     </div>
