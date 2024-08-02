351b9c34bec07fc873b0501a75eac573e7e54f21
AdeCandra12 <ade.can321@gmail.com>
2024-08-02 13:13:09 +0700

    Update Contact component with padding and form improvements

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,6 @@ const Contact = () => {
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
         <div className="mx-4 flex flex-wrap">
-          {/* Left column for the contact form */}
           <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
               className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
